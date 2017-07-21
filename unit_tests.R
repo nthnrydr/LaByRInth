fa42dae920c7ae2fa01fa23f4563cf8f76754054
Jason Vander Woude <jasonvwoude@gmail.com>
2017-07-21 08:07:34 -0500

    Add unit testing code

---
+++ unit_tests.R
@@ -0,0 +1,46 @@
+## Set up the testing preferences
+test.prefs <- list()
+class(test.prefs)             <- "prefs"
+test.prefs$markov.order       <- 5
+test.prefs$parents            <- c("LAKIN", "FULLER")
+test.prefs$resolve.conflicts  <- FALSE
+test.prefs$read.err           <- 0.05
+test.prefs$genotype.err       <- 0.05
+test.prefs$recomb.err <- 0.05
+test.prefs$recomb.dist <- 100000
+test.prefs$recomb.double <- FALSE
+test.prefs$window.size <- test.prefs$markov.order + 2
+test.prefs$min.samples <- prefs$window.size
+test.prefs$min.fraction <- NULL  # Don't remember what this is
+test.prefs$min.markers <- NULL # Don't remember what this is
+
+
+
+
+## Check the generatePath algorithm
+path.tracker.a <- matrix(c(3,1,1,1,1,1,3,2,2,2,3,3),byrow=T,nrow=3)
+path.tracker.a
+generatePath(path.tracker.a, 1)
+generatePath(path.tracker.a, 2)
+generatePath(path.tracker.a, 3)
+
+probs <- matrix(c(0.5,0.4,0.1,0.3,0.3,0.4,0.7,0.1,0.2,0.5,0.4,0.1),nrow=4,byrow=T)
+probs
+transProb <- function(a, b, dists, prefs) {}
+
+
+## Check
+entry <- "0/0:1,0:1:66:0,3,36"
+
+
+
+
+## Check vcf file creation
+alphabet <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l")
+reorder(alphabet, 3)
+array(reorder(alphabet, 3), dim=c(2,2,3))
+mat <- matrix(c("a:b:c", "d:e:f", "g:h:i", "j:k:l"), nrow=2)
+mat
+alphabet <- unlist(strsplit(mat, ":"))
+alphabet
+array(reorder(alphabet, 3), dim=c(2,2,3))
