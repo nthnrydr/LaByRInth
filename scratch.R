c65814118fd3b1e81e52a769eca9ba980ffd5303
Jason Vander Woude <jasonvwoude@gmail.com>
2017-07-21 08:23:19 -0500

    Add scratch file

---
+++ scratch.R
@@ -0,0 +1,4 @@
+source("http://bioconductor.org/biocLite.R") 
+biocLite("VariantAnnotation") #install the package
+library("VariantAnnotation") #load the package
+example("readVcf") #optional, test the function by running example codes
